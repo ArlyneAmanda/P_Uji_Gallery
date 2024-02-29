a196b16cada95492abc551a2e152fc3f09c4af9d
R3N-di <rendiramadhanmars@gmail.com>
2024-02-29 22:42:32 +0700

    salah ketik

--- aksi/album/aksi_hapus.php
+++ aksi/album/aksi_hapus.php
@@ -10,15 +10,15 @@ $dataFoto = query("SELECT * FROM foto WHERE album_id = '$album_id'");
 
 var_dump($dataFoto);
 
-// if($dataFoto != null){
-//     for($i = 0; $i < count($dataFoto); $i++){
-//         query("DELETE FROM album WHERE album_id = '$dataFoto[$i]['album_id']'");
-//     }
+if($dataFoto != null){
+    for($i = 0; $i < count($dataFoto); $i++){
+        query("DELETE FROM foto WHERE album_id = '$dataFoto[$i]['album_id']'");
+    }
 
-//     query("DELETE FROM album WHERE album_id = '$album_id'");
-// } else {
-//     header('location: ../../profile.php?errorFoto=true');
-// }
+    query("DELETE FROM album WHERE album_id = '$album_id'");
+} else {
+    header('location: ../../profile.php?errorFoto=true');
+}
 
 
 
